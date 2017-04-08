9cde77a0c18f54117dacdcb64050dcbf212760af
sachinsaner <sachinsaner@gmail.com>
2017-04-08 16:47:39 -0700

    Added practice code

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿namespace CodingPractice
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeOperations treeOperations = new TreeOperations();
+
+            //TreeNode root = new TreeNode(71);
+
+            //treeOperations.CreateBST(root, new TreeNode(41));
+            //treeOperations.CreateBST(root, new TreeNode(31));
+            //treeOperations.CreateBST(root, new TreeNode(35));
+            //treeOperations.CreateBST(root, new TreeNode(34));
+            //treeOperations.CreateBST(root, new TreeNode(36));
+            //treeOperations.CreateBST(root, new TreeNode(57));
+
+            TreeNode root = new TreeNode(15);
+
+            treeOperations.CreateBST(root, new TreeNode(10));
+            treeOperations.CreateBST(root, new TreeNode(20));
+            treeOperations.CreateBST(root, new TreeNode(5));
+            treeOperations.CreateBST(root, new TreeNode(13));
+            treeOperations.CreateBST(root, new TreeNode(17));
+            treeOperations.CreateBST(root, new TreeNode(35));
+
+            TreeNode prev = null;
+            TreeNode head = null;
+
+            treeOperations.BSTToDLL(root, ref prev, ref head);
+
+            TreeNode temp = head;
+            TreeNode lastNode = null;
+            while(temp != null)
+            {
+                Console.Write("\t" + temp.Value);
+                if(temp.Right == null)
+                {
+                    lastNode = temp;
+                }
+
+                temp = temp.Right;
+            }
+
+            Console.WriteLine();
+
+            //temp = head;
+            while (lastNode != null)
+            {
+                Console.Write("\t" + lastNode.Value);
+                lastNode = lastNode.Left;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ TreeOperations.cs
@@ -0,0 +1,178 @@
+﻿namespace CodingPractice
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class TreeOperations
+    {
+        //public static TreeNode prev;
+
+        public TreeNode CreateBST(TreeNode root, TreeNode newNode)
+        {
+            if (root == null)
+            {
+                return root = newNode;
+            }
+            if (newNode.Value < root.Value)
+            {
+                root.Left = CreateBST(root.Left, newNode);
+            }
+            else
+            {
+                root.Right = CreateBST(root.Right, newNode);
+            }
+
+            return root;
+        }
+
+        public TreeNode InsertIntoBST(TreeNode node, int data)
+        {
+            if (node == null)
+                node = new TreeNode(data);
+            else
+            {
+                if (data <= node.Value)
+                    node.Left = InsertIntoBST(node.Left, data);
+                else
+                    node.Right = InsertIntoBST(node.Right, data);
+            }
+            return node;
+        }
+
+        public bool ISBST(TreeNode root, ref TreeNode prev)
+        {
+            if (root != null)
+            {
+                if (!ISBST(root.Left, ref prev))
+                {
+                    return false;
+                }
+
+                if (prev != null && root.Value < prev.Value)
+                {
+                    return false;
+                }
+
+                int rootval1 = prev == null ? -1 : prev.Value;
+
+                Console.WriteLine("root value: " + root.Value + " prev value: " + rootval1);
+
+                prev = root;
+
+                if (!ISBST(root.Right, ref prev))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public void BSTToDLL(TreeNode root, ref TreeNode prev, ref TreeNode head)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            BSTToDLL(root.Left, ref prev, ref head);
+
+            if (prev != null && head == null)
+            {
+                head = prev;
+            }
+
+            if (prev != null)
+            {
+                prev.Right = root;
+                root.Left = prev;
+            }
+            prev = root;
+
+            BSTToDLL(root.Right, ref prev, ref head);
+        }
+
+        public void InorderIterative(TreeNode root)
+        {
+            if (root == null)
+                return;
+
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            bool done = true;
+            while (done)
+            {
+                if (root != null)
+                {
+                    stack.Push(root);
+                    root = root.Left;
+                }
+                else
+                {
+                    if (stack.Count != 0)
+                    {
+                        root = stack.Pop();
+                        Console.WriteLine(root.Value);
+
+                        root = root.Right;
+                    }
+                    else
+                    {
+                        done = false;
+                    }
+                }
+            }
+        }
+
+        public void BSTToDLL_Iterative(TreeNode root)
+        {
+            TreeNode prev = null;
+            TreeNode head = null;
+
+            if (root == null)
+                return;
+
+            var stack = new Stack<TreeNode>();
+            bool done = true;
+            while (done)
+            {
+                if (root != null)
+                {
+                    stack.Push(root);
+                    root = root.Left;
+                }
+                else
+                {
+                    if (stack.Count != 0)
+                    {
+                        root = stack.Pop();
+
+                        if (prev == null)
+                        {
+                            prev = root;
+                            head = root;
+                        }
+                        else
+                        {
+                            prev.Right = root;
+                            root.Left = prev;
+
+                            prev = prev.Right;
+                        }
+
+                        root = root.Right;
+                    }
+                    else
+                    {
+                        done = false;
+                    }
+                }
+            }
+
+            while (head != null)
+            {
+                Console.WriteLine(head.Value);
+                head = head.Right;
+            }
+        }
+    }
+}
