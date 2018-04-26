296a651366ca25ca811cc1595c501d4d239aecd2
sasaner <sasaner@microsoft.com>
2018-04-26 13:39:22 -0700

    formatting

--- TreeOperations.cs
+++ TreeOperations.cs
@@ -688,11 +688,19 @@
             {
                 return -1;
             }
+
             int leftHeight = checkHeight(root.Left);
-            if (leftHeight == int.MinValue) return int.MinValue; // Propagate error up
+
+            if (leftHeight == int.MinValue)
+            {
+                return int.MinValue; // Propagate error up
+            }
 
             int rightHeight = checkHeight(root.Right);
-            if (rightHeight == int.MinValue) return int.MinValue; // Propagate error up
+            if (rightHeight == int.MinValue)
+            {
+                return int.MinValue; // Propagate error up
+            }
 
             int heightDiff = leftHeight - rightHeight;
             if (Math.Abs(heightDiff) > 1)
@@ -762,6 +770,28 @@
         }
 
         //https://leetcode.com/problems/binary-tree-longest-consecutive-sequence/description/
+        /*
+         * Given a binary tree, find the length of the longest consecutive sequence path.
+           The path refers to any sequence of nodes from some starting node to any node in the tree along the parent-child connections.
+           The longest consecutive path need to be from parent to child (cannot be the reverse).
+           For example,
+               1
+                \
+                 3
+                / \
+               2   4
+                    \
+                     5
+            Longest consecutive sequence path is 3-4-5, so return 3.
+               2
+                \
+                 3
+                / 
+               2    
+              / 
+             1
+            Longest consecutive sequence path is 2-3,not3-2-1, so return 2.
+        */
         public int MaxConsecutiveSequence(TreeNode root)
         {
             int maxSeq = 0;
